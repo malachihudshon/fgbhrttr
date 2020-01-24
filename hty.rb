058fdbf8cbcbf28e6b62e88aeb0946a888e79d66
malachihudshon <37843505+malachihudshon@users.noreply.github.com>
2020-01-24 14:36:02 -0800

    Add files via upload

---
+++ hty.rb
@@ -0,0 +1,103 @@
+sample"C:/Users/malachi_hudshon/Downloads/challenge_d (1)/mario_sounds/mario_intro.wav"
+sleep 3
+live_loop :second do
+  play:e5
+  sleep 0.25
+  play:e5
+  sleep 0.25
+  play:e5
+  sleep 0.25
+  play:c5
+  sleep 0.25
+  play:e5
+  sleep 0.25
+  play:g5
+  sleep 0.25
+  play:c5
+  sleep 0.25
+  play:g4
+  sleep 0.25
+  play:e4
+  sleep 0.25
+  play:a4
+  sleep 0.25
+  play:b4
+  sleep 0.25
+  play:Bb4
+  sleep 0.25
+  play:a4
+  play:g5
+  sleep 0.25
+  play:fs5
+  sleep 0.25
+  play:f5
+  sleep 0.25
+  play:ds5
+  sleep 0.25
+  play:e5
+  sleep 0.25
+  play:gs4
+  sleep 0.25
+  play:a4
+  sleep 0.25
+  play:c5
+  sleep 0.25
+  play:a4
+  sleep 0.25
+  play:c5
+  sleep 0.25
+  play:d5
+  sleep 0.25
+  play:g5
+  sleep 0.25
+  play:fs5
+  sleep 0.25
+  play:f5
+  sleep 0.25
+  play:ds5
+  sleep 0.25
+  play:e5
+  sleep 0.25
+  play:c6
+  sleep 0.25
+  play:c6
+  sleep 0.25
+  play:c6
+  sleep 0.25
+  play:g5
+  sleep 0.25
+  play:fs5
+  sleep 0.25
+  play:f5
+  sleep 0.25
+  play:ds5
+  sleep 0.25
+  play:e5
+  sleep 0.25
+  play:gs4
+  sleep 0.25
+  play:a4
+  sleep 0.25
+  play:c5
+  sleep 0.25
+  play:a4
+  sleep 0.25
+  play:c5
+  sleep 0.25
+  play:d5
+  sleep 0.25
+  play:ds5
+  sleep 0.25
+  play:d5
+  sleep 0.25
+  play:c5
+  sleep 0.25
+end
+live_loop :po do
+  sleep 5
+  sample "C:/Users/malachi_hudshon/Downloads/challenge_d (1)/mario_sounds/coin.wav"
+  
+  
+  
+end
+
